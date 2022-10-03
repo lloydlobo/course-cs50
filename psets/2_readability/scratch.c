dadf48fde9cb8363c1ae5aa055e0299d366ac8ea
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-10-03 17:56:36 +0530

    chore(readability): add testcases examples

--- psets/2_readability/scratch.c
+++ psets/2_readability/scratch.c
@@ -27,7 +27,48 @@
 void prompt(void);
 // char *round_it(float f);
 
-const char *STR = "One fish. Red fish. Blue fish. Yellow fish.";
+// G_1, G_2, G_3, G_5, G_7, G_8_0, G_8_1, G_9, G_10, G_16_PLUS :=
+// const char *STR_1 = "One fish. Red fish. Blue fish. Yellow fish.";
+const char *STR_1 = "One fish. Two fish. Red fish. Blue fish.";
+const char *STR_2 = "Would you like them here or there? I would not like them "
+                    "here or there. I would not like them anywhere.";
+const char *STR_3 = "Congratulations! Today is your day. You're off to Great "
+                    "Places! You're off and away!";
+const char *STR_5 =
+    "Harry Potter was a highly unusual boy in many ways. For one thing, he "
+    "hated the summer holidays more than any other time of year. For another, "
+    "he really wanted to do his homework, but was forced to do it in secret, "
+    "in the dead of the night. And he also happened to be a wizard.";
+const char *STR_7 =
+    "In my younger and more vulnerable years my father gave me some advice "
+    "that I've been turning over in my mind ever since.";
+const char *STR_8_0 =
+    "Alice was beginning to get very tired of sitting by her sister on the "
+    "bank, and of having nothing to do: once or twice she had peeped into the "
+    "book her sister was reading, but it had no pictures or conversations in "
+    "it, \"and what is the use of a book,\" thought Alice \"without pictures "
+    "or conversation?\"";
+const char *STR_8_1 =
+    "When he was nearly thirteen, my brother Jem got his arm badly broken at "
+    "the elbow. When it healed, and Jem's fears of never being able to play "
+    "football were assuaged, he was seldom self-conscious about his injury. "
+    "His left arm was somewhat shorter than his right; when he stood or "
+    "walked, the back of his hand was at right angles to his body, his thumb "
+    "parallel to his thigh.";
+const char *STR_9 = "There are more things in Heaven and Earth, Horatio, than "
+                    "are dreamt of in your philosophy.";
+const char *STR_10 =
+    "It was a bright cold day in April, and the clocks were striking thirteen. "
+    "Winston Smith, his chin nuzzled into his breast in an effort to escape "
+    "the vile wind, slipped quickly through the glass doors of Victory "
+    "Mansions, though not quickly enough to prevent a swirl of gritty dust "
+    "from entering along with him.";
+const char *STR_16 =
+    "A large class of computational problems involve the determination of "
+    "properties of graphs, digraphs, integers, arrays of integers, finite "
+    "families of finite sets, boolean formulas and elements of other countable "
+    "domains.";
+
 const float PER_100_WORDS = 100.0;
 const char SPACE = 32;
 const char PERIOD = '.';
@@ -35,7 +76,7 @@ const char EXCLAMATION = '!';
 
 int main(int argc, char *argv[]) {
   int malloc;
-  malloc = strlen(STR); // allocate memory by SRL strlen size.
+  malloc = strlen(STR_1); // allocate memory by SRL strlen size.
 
   const int MALLOC = 100;
   const int MALLOC_OUT = 16;
@@ -54,7 +95,7 @@ int main(int argc, char *argv[]) {
   // TODO: Remove this later. For direct len calculation.
   // string_count = strlen(STR);
   for (int i = 0; i < MALLOC + 1; i++) {
-    string_user[i] = &STR[i];
+    string_user[i] = &STR_1[i];
   }
   string_count = strlen(*string_user);
 
@@ -64,7 +105,7 @@ int main(int argc, char *argv[]) {
   char str[string_count]; // Declare array to copy lower string.
 
   for (int i = 0; i < string_count + 1; i++) {
-    int c = STR[i];
+    int c = STR_1[i];
     char l = tolower(c);
     bool is_between_a_z = l >= 97 && l <= 122;
 
@@ -125,7 +166,7 @@ int main(int argc, char *argv[]) {
 
   char f_out[MALLOC];
   for (int i = 0; i <= MALLOC; i++) {
-    f_out[i] = sprintf(f_out, "Text: %s\n%s", STR, sprint_grade);
+    f_out[i] = sprintf(f_out, "Text: %s\n%s", STR_1, sprint_grade);
   }
 
   printf("%s\n\n", str);
