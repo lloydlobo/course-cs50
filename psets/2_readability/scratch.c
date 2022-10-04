29f32fa5578c9582fb1e1b699f30570ae9fbe9f0
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-10-04 11:23:17 +0530

    fix(readability): patch word count for loop

--- psets/2_readability/scratch.c
+++ psets/2_readability/scratch.c
@@ -8,40 +8,20 @@
 #define BOOL bool;
 
 /*
- * It's in the standard library, and that's the most straight forward way
- * I can see to implement such a function. So yes, just loop through the
- * string and convert each character to lowercase.
- *
- * Something trivial like this:
+ * CONSTANTS.
  */
-/* for(int i = 0; str[i]; i++){
-  str[i] = tolower(str[i]);
-} */
-/*
- * or if you prefer one liners, then you can use this one by J.F.
- * Sebastian:
- */
-// for ( ; *p; ++p) *p = tolower(*p);
-/* [Earlz] [so/q/2661766] [cc by-sa 3.0] */
-
-void prompt(void);
-// char *round_it(float f);
-
-// G_1, G_2, G_3, G_5, G_7, G_8_0, G_8_1, G_9, G_10, G_16_PLUS :=
-// const char *STR_1 = "One fish. Red fish. Blue fish. Yellow fish.";
-
+const int MALLOC = 100;
+const int MALLOC_OUT = 16;
 const float PER_100_WORDS = 100.0;
 const char SPACE = 32;
 const char PERIOD = '.';
 const char EXCLAMATION = '!';
 const char QUESTION = '?';
 
-const int MALLOC = 100;
-const int MALLOC_OUT = 16;
-
 /*
- * Forward declaration
+ * Forward declaration for functions.
  */
+void prompt(void);
 const char *get_testcases(); // Without prototype.
 
 /*
@@ -54,15 +34,11 @@ int main(int argc, char *argv[]) {
 
   int string_count = 0;
   int letter_count = 0;
-  int word_count;
+  int word_count = 1;
   int sentence_count;
-  int word_counter = 1;
-  int sentence_counter = 0;
 
-  /* assign string string count. */
+  /* assign string count. */
 
-  // TODO: Remove this later. For direct len calculation.
-  // string_count = strlen(STR);
   for (int i = 0; i < MALLOC + 1; i++) {
     string_user[i] = &INPUT_STR[i];
   }
@@ -70,7 +46,6 @@ int main(int argc, char *argv[]) {
 
   /* lower string count. */
 
-  // NOTE: Do we need lower case string?
   char str[string_count]; // Declare array to copy lower string.
 
   for (int i = 0; i < string_count + 1; i++) {
@@ -87,13 +62,11 @@ int main(int argc, char *argv[]) {
   /* word count. */
 
   for (int i = 0; i < string_count; i++) {
-    // char c = *string_user[i];
-    char c = str[i];
-    if (c == SPACE || c == EXCLAMATION || c == QUESTION) {
-      word_counter++;
+    char *c = &str[i];
+    if (*c == SPACE) {
+      word_count++;
     }
   }
-  word_count = word_counter;
 
   /* sentence count. */
 
@@ -101,10 +74,9 @@ int main(int argc, char *argv[]) {
     // char c = *string_user[i];
     char c = str[i];
     if (c == EXCLAMATION || c == QUESTION || c == PERIOD) {
-      sentence_counter++;
+      sentence_count++;
     }
   }
-  sentence_count = sentence_counter;
 
   /* Average Count */
 
@@ -113,9 +85,8 @@ int main(int argc, char *argv[]) {
 
   /* Coleman-Liau Index */
 
-  // grade := 0.0588*l - 0.296*s - 15.8
-  float calculate_grade =
-      roundf(0.0588 * letter_avg - 0.296 * sentence_avg - 15.8);
+  float calculate_grade = roundf(0.0588 * letter_avg - 0.296 * sentence_avg -
+                                 15.8); // grade := 0.0588*l - 0.296*s - 15.8
 
   char sprint_grade[MALLOC];
 
@@ -144,11 +115,12 @@ int main(int argc, char *argv[]) {
 
   /* Print results. */
 
+  // TODO:
+  // FIX: this. It disrupts all the printf above.
   // char f_out[MALLOC];
   // for (int i = 0; i <= MALLOC; i++) {
   //   f_out[i] = sprintf(f_out, "Text: %s\n%s", INPUT_STR, sprint_grade);
   // }
-
   // printf("\n%s\n", f_out);
 
   return 0;
@@ -162,8 +134,10 @@ void prompt(void) {
 }
 // for (int i = 0; i < strlen(msg_1); i++) { out[i] = msg_1[i]; }
 
+/*
+ * get_testcases function returns a string to grade.
+ */
 const char *get_testcases() {
-
   const char *STR_1 = "One fish. Two fish. Red fish. Blue fish.";
   const char *STR_2 =
       "Would you like them here or there? I would not like them "
@@ -174,7 +148,8 @@ const char *get_testcases() {
       "Harry Potter was a highly unusual boy in many ways. For one thing, he "
       "hated the summer holidays more than any other time of year. For "
       "another, "
-      "he really wanted to do his homework, but was forced to do it in secret, "
+      "he really wanted to do his homework, but was forced to do it in "
+      "secret, "
       "in the dead of the night. And he also happened to be a wizard.";
   const char *STR_7 =
       "In my younger and more vulnerable years my father gave me some advice "
@@ -183,15 +158,21 @@ const char *get_testcases() {
       "Alice was beginning to get very tired of sitting by her sister on the "
       "bank, and of having nothing to do: once or twice she had peeped into "
       "the "
-      "book her sister was reading, but it had no pictures or conversations in "
-      "it, \"and what is the use of a book,\" thought Alice \"without pictures "
+      "book her sister was reading, but it had no pictures or conversations "
+      "in "
+      "it, \"and what is the use of a book,\" thought Alice \"without "
+      "pictures "
       "or conversation?\"";
   const char *STR_8_1 =
-      "When he was nearly thirteen, my brother Jem got his arm badly broken at "
-      "the elbow. When it healed, and Jem's fears of never being able to play "
-      "football were assuaged, he was seldom self-conscious about his injury. "
+      "When he was nearly thirteen, my brother Jem got his arm badly broken "
+      "at "
+      "the elbow. When it healed, and Jem's fears of never being able to "
+      "play "
+      "football were assuaged, he was seldom self-conscious about his "
+      "injury. "
       "His left arm was somewhat shorter than his right; when he stood or "
-      "walked, the back of his hand was at right angles to his body, his thumb "
+      "walked, the back of his hand was at right angles to his body, his "
+      "thumb "
       "parallel to his thigh.";
   const char *STR_9 =
       "There are more things in Heaven and Earth, Horatio, than "
@@ -199,7 +180,8 @@ const char *get_testcases() {
   const char *STR_10 =
       "It was a bright cold day in April, and the clocks were striking "
       "thirteen. "
-      "Winston Smith, his chin nuzzled into his breast in an effort to escape "
+      "Winston Smith, his chin nuzzled into his breast in an effort to "
+      "escape "
       "the vile wind, slipped quickly through the glass doors of Victory "
       "Mansions, though not quickly enough to prevent a swirl of gritty dust "
       "from entering along with him.";
@@ -210,5 +192,5 @@ const char *get_testcases() {
       "countable "
       "domains.";
 
-  return STR_2;
+  return STR_3;
 }
