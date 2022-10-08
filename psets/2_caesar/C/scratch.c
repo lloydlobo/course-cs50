5322bdaf6f06d187b4cfca23078851cf73f444b8
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-10-08 16:17:48 +0530

    feat(caesar): add function char key to int argv

--- psets/2_caesar/C/scratch.c
+++ psets/2_caesar/C/scratch.c
@@ -9,39 +9,40 @@
 void print_line(void);
 void report_argv_len_not_enough();
 void report_argv_len_more_than_expected();
-void report_argv_1_not_int();
+void report_argv_1_key_not_int();
 void report_argv_2_not_a_string();
 void report_successful_input(char *argv[], char *cyp[]);
 bool only_digits(char *s[]);
+int cli_argv_key_to_int(char *argv[]);
 
 /* Design and implement a program, caesar,
    that encrypts messages using Caesar’s cipher. */
 int main(int argc, char *argv[]) {
-  /* Handle if no input is added. */
-  if (argc < 2) {
+  if (argc < 2) {                 /* Handle if no input is added. */
     report_argv_len_not_enough(); // TODO: Cover case fo argc == 1. no value.
     return EXIT_FAILURE;          // exit.
   } else if (argc > 3) {
     report_argv_len_more_than_expected();
     return EXIT_FAILURE; // exit.
-  }
-  /* TODO: Ensure no line breaks in cli argument */
-
-  /* TODO: Tyoe check arg[1] to be a digit or int. */
+  }                      /* TODO: Ensure no line breaks in cli argument */
+                         /* TODO: Tyoe check arg[1] to be a digit or int. */
   if (only_digits(argv) == 0) {
-    report_argv_1_not_int();
+    report_argv_1_key_not_int();
     return EXIT_FAILURE;
   }
-
   /* argument count & vector. */
-
   int len_argc_bin = strlen(argv[0]);   // ./scratch.
   int len_argc_key = strlen(argv[1]);   // 13.
   int len_argc_value = strlen(argv[2]); // hello.
   int len_argc_last = strlen(argv[argc - 1]);
 
+  int k = cli_argv_key_to_int(&argv[1]);
+
+  /* TODO: PROMPT User for plaintext */
+
   /* Get input key/crypt */
   char key_temp[len_argc_key];
+  printf("k: %i\n", k);
   /* Convert key from char[] to int. */
   for (int i = 0; i < len_argc_key; i++) {
     char *c = &argv[1][i];
@@ -50,19 +51,18 @@ int main(int argc, char *argv[]) {
   int key = atoi(key_temp); // int *key = (int *)key_temp; // 13 => 13105.
                             // TODO: if key is a float retrun exit code 1.
   if (key < 1) {            // argv[1] is a +ve integer.
-    report_argv_1_not_int();
+    report_argv_1_key_not_int();
     return EXIT_FAILURE;
-  }
-
-  /* TODO: PROMPT User for plaintext */
+  } /* TODO: PROMPT User for plaintext */
 
   /* Get input password value */
-  // Unicode values: A-Z == 64-96 | a-z == 97-122.
-  // PERF: Add support for english chars. ?!,:
-  // PERF: else statement: Doing 2 things. Collect & error handiling. Refactor.
-  // TODO: See when to use lowercase?
+
   char value_temp[len_argc_value];
 
+  /* Unicode values: A-Z == 64-96 | a-z == 97-122.
+      PERF: Add support for english chars. ?!,:
+      PERF: else statement: Doing 2 things. Collect & error handling.
+      TODO: See when to use lowercase? */
   for (int i = 0; i < len_argc_value; i++) {
     char *c = &argv[2][i];
     bool is_AZ_az = *c >= 64 && *c <= 122;
@@ -71,30 +71,29 @@ int main(int argc, char *argv[]) {
       value_temp[i] = *c;
     } else {
       report_argv_2_not_a_string();
-      return 1;
-      // return EXIT_FAILURE;
+      return EXIT_FAILURE; // return 1;
     }
   }
+
   const char *value = value_temp;
-
   bool is_not_equal_to_argv = strcmp(value, argv[2]) != 0;
-  if (is_not_equal_to_argv) {
+  if (is_not_equal_to_argv)
     return EXIT_FAILURE;
-  }
 
+  /* Convert key to 1-26 Caesar crypt. Cycles at 26 a-z. */
   /* plaintext     H     E     L     L     O
      + key         \(1\) \(1\) \(1\) \(1\) \(1\)
      = ciphertext  I     F     M     M     P */
-  /* Convert key to 1-26 Caesar crypt. Cycles at 26 a-z. */
-  // A = 0, B = 1, ... H = 7, e = 10
-  // TODO: Is conversion of key < 26 needed here?
-  // https://scholarsoul.com/convert-int-to-char-in-c/
+  /* A = 0, B = 1, ... H = 7, e = 10
+     TODO: Is conversion of key < 26 needed here?
+     https://scholarsoul.com/convert-int-to-char-in-c/ */
+
   char cypher[len_argc_value];
   char c, s, no_buf_overrun[2]; // Keep capacity above 1.
-  /* TODO: For each char in plaintext rotate by key i.e. caesar cypher*/
+  /* TODO: For each char in plaintext,
+     rotate by key i.e. caesar cypher */
   for (int i = 0; i < len_argc_value; i++) {
     int char_ascii_code = value[i]; // 72 101 108 108 79
-    /* TODO: For each char in plaintext rotate if it's a letter */
     if (char_ascii_code < 92)
       printf("less:    %i\n", char_ascii_code);
     else
@@ -104,6 +103,9 @@ int main(int argc, char *argv[]) {
       c = 'A' + cypher_value - 10;
     else
       c = '0' + cypher_value;
+
+    /* TODO: rotate if it's a letter */
+
     /* • If you’re not seeing any output at all when you call printf, odds are
       it’s because you’re printing characters outside of the valid ASCII range
       from 0 to 127. Try printing characters temporarily as numbers (using %i
@@ -155,15 +157,26 @@ bool only_digits(char *s[]) {
   }
   return true;
 }
+/* char to int
+   TODO: if key is a float retrun exit code 1.
+   1. sscanf() 2. atoi() 3. Typecasting. string => int. */
+int cli_argv_key_to_int(char *arg1[]) {
+  int len_argv_key = strlen(arg1[0]);
+  char key_str[len_argv_key];
+  for (int i = 0; i < len_argv_key; i++) {
+    char *c = &arg1[0][i];
+    key_str[i] = *c;
+  }
+  /* Convert key from char[] to int. */
+  int key = atoi(key_str);
+  if (key < 1) {
+    report_argv_1_key_not_int();
+    return EXIT_FAILURE; // exit code 1.
+  }
+  return key;
+}
 
 // https://cs50.harvard.edu/x/2022/psets/2/caesar/
-/* More formally, Caesar’s algorithm (i.e., cipher) encrypts messages by
-   “rotating” each letter by positions. More formally, if is some plaintext
-   (i.e., an unencrypted message),
-   pi is the ith character in p, k and is a secret key (i.e., a non-negative
-   integer), then each letter, ci, in the ciphertext, c, is computed as:
-
-   ci = (pi + k) % 26 */
 
 // https://scholarsoul.com/convert-int-to-char-in-c/
 /* Characters use an encoding (typically ASCII) to map numbers to a
@@ -187,7 +200,7 @@ void report_argv_len_more_than_expected(void) {
          "     ./caesar 13 Hello\n");
 }
 
-void report_argv_1_not_int(void) {
+void report_argv_1_key_not_int(void) {
   printf("err: Entered key is not a valid positive integer.\n\n"
          "Usage:\n"
          "     ./caesar <key> <password>\n"
