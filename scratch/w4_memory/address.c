cc1b5f16b2ac42486f6675373940e282f2034a1f
Lloyd Lobo <lloydlobo4@gmail.com>
2023-09-12 10:34:46 +0530

    .

--- scratch/w4_memory/address.c
+++ scratch/w4_memory/address.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // Pointer Arithmetic:: 01:05:22
 int main(int argc, char *argv[]) {
@@ -12,5 +13,33 @@ int main(int argc, char *argv[]) {
     printf("%i ", *(numbers + i));
   }
   printf("\n");
+
+  int n = 50;
+  int *p = &n;
+  printf("`*p = &n` => %i\n", *p);
+
+  // %p print pointer, %s go to pointer at string and print consecutive
+  // till null-terminator.
+  const char *string = "Hello, World!";
+  // printf internally knows to go to pointer *string by default. adding `*` to
+  // string will make it print just the char at the address.
+  printf("%s\n", string);
+  // Pointer arithmetic.
+  printf("%c\n", *(string));     // H
+  printf("%c\n", *(string + 1)); // e
+  printf("%c\n", *(string + 2)); // l
+  // NOTE: For other data types, Compiler knows how many bytes to add for
+  // checking next next... etc.
+  printf("%s\n", (string));     // Hello, World!
+  printf("%s\n", (string + 1)); // ello, World!
+  printf("%s\n", (string + 2)); // llo, World!
+  // printf("%c\n", *(string + 20000)); segmentation fault (core dumped)
+  for (int i = 0; i < strlen(string); i++) {
+    // `&` gives address of the values.
+    printf("%p <- ", &(string[i]));
+    printf("%c\n", string[i]);
+  }
+  printf("\n");
+
   return 0;
 }
--- scratch/w4_memory/memory.c
+++ scratch/w4_memory/memory.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 
 void swap(int *x, int *y);
+
+/* $ gcc memory.c -o memory */
 int main(int argc, char *argv[]) {
   int *x = malloc(3 * sizeof(int));
+
   x[1] = 72;
   x[2] = 73;
   x[3] = 33;
   printf("%i\n", *x);
+
   int a = 11;
   int b = *(&a) * 9;
   swap(&a, &b);
   printf("a: %i; b: %i\n", a, b);
+
   return 0;
 }
 
