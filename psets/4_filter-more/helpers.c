7f4cc4d25c0233a2ce395a2c40e23899c859225c
Lloyd Lobo <lloydlobo4@gmail.com>
2023-09-14 12:17:14 +0530

    feat: implement grayscale helper func
    We average the RGB BYTE (uint8_t) of each pixel,
    in the image by looping for each pixel across:
    image pixelated columns and for each image rows.
    
    Averaging allows to keep uniform contrast and,
    brightness levels.

--- psets/4_filter-more/helpers.c
+++ psets/4_filter-more/helpers.c
@@ -1,8 +1,21 @@
 #include "helpers.h"
+#include <math.h>
+#include <stdio.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            // typedef uint8_t BYTE;
+            BYTE avg_rgb = (image[y][x].rgbtRed + image[y][x].rgbtGreen + image[y][x].rgbtBlue) / 3;
+
+            // Assign the average value to all components
+            image[y][x].rgbtRed = avg_rgb;
+            image[y][x].rgbtGreen = avg_rgb;
+            image[y][x].rgbtBlue = avg_rgb;
+        }
+    }
     return;
 }
 
