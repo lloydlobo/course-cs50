b4e883e4de971971b8307715023d475dd0800668
Lloyd Lobo <lloydlobo4@gmail.com>
2023-09-14 10:05:34 +0530

    .

--- problems/4_license/license.c
+++ problems/4_license/license.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void free_array(char **array, int size) {
+  if (array == NULL) {
+    return;
+  }
+  for (int i = 0; i < size; i++) {
+    free(array[i]);
+  }
+  // free(array);
+}
 
 int main(int argc, char *argv[]) {
   // Check for command line args
@@ -14,6 +26,10 @@ int main(int argc, char *argv[]) {
   char *plates[8];
 
   FILE *infile = fopen(argv[1], "r");
+  if (infile == NULL) {
+    perror("Could not open file.\n");
+    return 1;
+  }
 
   int idx = 0;
 
@@ -22,12 +38,42 @@ int main(int argc, char *argv[]) {
     buffer[6] = '\0';
 
     // Save plate number in array
-    plates[idx] = buffer;
-    printf("%s\n", plates[idx]);
+    plates[idx] = malloc(strlen(buffer) * sizeof(char *));
+    strcpy(plates[idx], buffer); // plates[idx] = buffer;
+
     idx++;
   }
 
   for (int i = 0; i < 8; i++) {
     printf("%s\n", plates[i]);
   }
+
+  free_array(plates, 8);
+  fclose(infile);
 }
+
+/*
+10:01am zsh $ valgrind ./license plates.txt
+==444077== Memcheck, a memory error detector
+==444077== Copyright (C) 2002-2022, and GNU GPL'd, by Julian Seward et al.
+==444077== Using Valgrind-3.21.0 and LibVEX; rerun with -h for copyright info
+==444077== Command: ./license plates.txt
+==444077==
+11ZT00
+1KAD21
+78ZZ01
+99ZZ11
+72ZZ21
+98ZZ31
+44ZW41
+34ZZ51
+==444077==
+==444077== HEAP SUMMARY:
+==444077==     in use at exit: 0 bytes in 0 blocks
+==444077==   total heap usage: 11 allocs, 11 frees, 5,976 bytes allocated
+==444077==
+==444077== All heap blocks were freed -- no leaks are possible
+==444077==
+==444077== For lists of detected and suppressed errors, rerun with: -s
+==444077== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+*/
